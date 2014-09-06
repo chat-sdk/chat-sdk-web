4afa1dfbb04301c6926b0252653623af29e881c9
Benjamin Smiley-andrews <benjaminsmiley-andrews@Benjamins-MacBook-Pro.local>
2014-09-06 12:14:45 +0100

    Fix #101 - Moved the online list to a new branch

--- app/js/utilities.js
+++ app/js/utilities.js
@@ -89,7 +89,7 @@ var Paths = {
     },
 
     onlineUsersRef: function () {
-        return this.usersRef().child(bOnlineKey);
+        return this.firebase().child(bOnlineKey);
     },
 
     onlineUserRef: function (fid) {
