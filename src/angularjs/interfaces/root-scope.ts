f8d3d48de99ff74310d67fa80b4c529a1a83b44d
Pepe Becker <mail@pepebecker.com>
2019-09-10 21:18:03 +0900

    Update DisableDrag directive

--- src/angularjs/interfaces/root-scope.ts
+++ src/angularjs/interfaces/root-scope.ts
@@ -4,4 +4,5 @@ import {UserDragAction} from "../directives/draggable-user";
 export interface IRootScope extends ng.IRootScopeService {
     user: IUser
     userDrag: UserDragAction
+    disableDrag: boolean
 }
