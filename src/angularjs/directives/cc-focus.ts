ae65f83a15008b6a14dc2db3bd1178d8906cb3d0
Pepe Becker <mail@pepebecker.com>
2019-09-10 21:13:53 +0900

    Update CCFocus directive

--- src/angularjs/directives/cc-focus.ts
+++ src/angularjs/directives/cc-focus.ts
@@ -1,15 +1,25 @@
-import * as angular from 'angular'
+import * as angular from 'angular';
 
+export interface ICCFocus extends ng.IDirective {
 
-angular.module('myApp.directives').directive('ccFocus', function () {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attr) {
-            scope.$watch(attr.ccFocus, (n, o) => {
-                if (n !== 0 && n) {
-                    element[0].focus();
-                }
-            });
-        }
-    };
-});
+}
+
+class CCFocus implements ICCFocus {
+
+    restrict = 'A';
+
+    link(scope: ng.IScope, element: JQLite, attr: ng.IAttributes) {
+        scope.$watch(attr.ccFocus, (n, o) => {
+            if (n !== 0 && n) {
+                element[0].focus();
+            }
+        });
+    }
+
+    static factory(): ng.IDirectiveFactory {
+        return () => new CCFocus()
+    }
+
+}
+
+angular.module('myApp.directives').directive('ccFocus', CCFocus.factory());
