f8bdfa75206707f83ab0a0e7ca510ab4f38cc549
Pepe Becker <mail@pepebecker.com>
2019-09-10 21:15:10 +0900

    Update CenterMouseY directive

--- src/angularjs/directives/center-mouse-y.ts
+++ src/angularjs/directives/center-mouse-y.ts
@@ -1,27 +1,45 @@
-// This is used by the profile box - to keep it centered on the
-// mouse's y axis until we move into it
-import * as $ from 'jquery'
-import * as angular from 'angular'
-import {IProfileBoxScope} from "../controllers/user-profile-box";
+import * as $ from 'jquery';
+import * as angular from 'angular';
 
+import { IProfileBoxScope } from '../controllers/user-profile-box';
+import { IScreen } from '../services/screen';
 
-angular.module('myApp.directives').directive('centerMouseY', ['$document', 'Screen', function ($document, Screen) {
-    return {
-        link: function (scope: IProfileBoxScope, elm) {
+export interface ICenterMouseY extends ng.IDirective {
 
-            $(elm).hover(() => {
-                scope.hover = true;
-            }, () => {
-                scope.hover = false;
-            });
+}
 
-            $(document).mousemove((e) => {
-                //!elm.is(":hover")
-                if (scope.currentUser && !scope.hover) {
-                    // Keep the center of this box level with the mouse y
-                    elm.css({bottom: (Screen.screenHeight - e.clientY - $(elm).height() / 2) + 'px'});
-                }
-            });
-        }
-    };
-}]);
+/**
+ * This is used by the profile box - to keep it centered on the mouse's y axis until we move into it
+ */
+class CenterMouseY implements ICenterMouseY {
+
+    static $inject = ['$document', 'Screen'];
+
+    constructor(
+        private $document: ng.IDocumentService,
+        private Screen: IScreen,
+    ) { }
+
+    link(scope: IProfileBoxScope, element: JQLite) {
+        $(element).hover(() => {
+            scope.hover = true;
+        }, () => {
+            scope.hover = false;
+        });
+
+        $(document).mousemove((e) => {
+            //!element.is(':hover')
+            if (scope.currentUser && !scope.hover) {
+                // Keep the center of this box level with the mouse y
+                element.css({bottom: (this.Screen.screenHeight - e.clientY - $(element).height() / 2) + 'px'});
+            }
+        });
+    }
+
+    static factory(): ng.IDirectiveFactory {
+        return ($document: ng.IDocumentService, Screen: IScreen) => new CenterMouseY($document, Screen);
+    }
+
+}
+
+angular.module('myApp.directives').directive('centerMouseY', CenterMouseY.factory());
