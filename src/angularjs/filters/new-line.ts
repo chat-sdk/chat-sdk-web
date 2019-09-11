a6ae8a8e182d8d2dde4d659293ec94e827f2804f
Pepe Becker <mail@pepebecker.com>
2019-09-11 19:44:28 +0900

    Update NewLineFilter

--- src/angularjs/filters/new-line.ts
+++ src/angularjs/filters/new-line.ts
@@ -1,11 +1,14 @@
-import * as angular from 'angular'
-angular.module('myApp.filters').filter('newline', function () {
-    return function(text) {
-        text = String(text);
+import * as angular from 'angular';
+
+function NewLineFilter(): ng.IFilterFunction {
+    return (input: string) => {
+        let text = String(input);
         text = text.split("\r").join("<br\>");
         text = text.split("\r\n").join("<br\>");
         text = text.split("\n").join("<br\>");
         text = text.split("&#10;").join("<br\>");
         return text;
-    };
-});
+    }
+}
+
+angular.module('myApp.filters').filter('newline', NewLineFilter);
