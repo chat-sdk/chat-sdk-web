83417024c6fe1077d56565b293c842ab014bf50b
Pepe Becker <mail@pepebecker.com>
2019-09-11 17:50:09 +0900

    Update SocialIFrame directive

--- src/angularjs/directives/social-iframe.ts
+++ src/angularjs/directives/social-iframe.ts
@@ -1,42 +1,62 @@
-import * as $ from 'jquery'
-import * as angular from 'angular'
+import * as $ from 'jquery';
+import * as angular from 'angular';
 
+import { N } from '../keys/notification-keys';
+import { IPaths } from '../network/paths';
+import { IRootScope } from '../interfaces/root-scope';
 
-import {N} from "../keys/notification-keys";
+export interface ISocialIFrame extends ng.IDirective {
 
-angular.module('myApp.directives').directive('socialIframe', ["$rootScope", "$document", "$window", "Paths", function ($rootScope, $document, $window, Paths) {
-    return {
-        link: function (scope, element, attr) {
+}
 
-            $rootScope.$on(N.StartSocialLogin, (event, data, callback) => {
+class SocialIFrame implements ISocialIFrame {
 
-                //element.load(function () {
+    $inject = ['$rootScope', /*'$document',*/ '$window'/*, 'Paths'*/];
+
+    constructor(
+        private $rootScope: IRootScope,
+        // private $document: ng.IDocumentService,
+        private $window: ng.IWindowService,
+        // private Paths: IPaths
+    ) { }
+
+    link(scope: ng.IScope, element: JQLite) {
+        this.$rootScope.$on(N.StartSocialLogin, (event, data, callback) => {
+
+            //element.load(function () {
 
 //                let data = {
 //                    action: 'github',
-//                    path: Paths.firebase().toString()
+//                    path: this.Paths.firebase().toString()
 //                };
 
-                // Add the event listener
-                let eventMethod = $window.addEventListener ? "addEventListener" : "attachEvent";
-                let eventer = $window[eventMethod];
-                let messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+            // Add the event listener
+            let eventMethod = this.$window.addEventListener ? 'addEventListener' : 'attachEvent';
+            let eventer = this.$window[eventMethod];
+            let messageEvent = eventMethod == 'attachEvent' ? 'onmessage' : 'message';
 
-                eventer(messageEvent, (e) => {
-                    if (e.data) {
-                        let data = JSON.parse(e.data);
-                        if (data.provider == 'chatcat') {
-                            callback(data);
-                        }
+            eventer(messageEvent, (e) => {
+                if (e.data) {
+                    let data = JSON.parse(e.data);
+                    if (data.provider == 'chatcat') {
+                        callback(data);
                     }
+                }
 //                    else {
 //                        callback(null);
 //                    }
-                });
-
-                $(element).get(0).contentWindow.postMessage(JSON.stringify(data), "*");
-                //});
             });
-        }
-    };
-}]);
+
+            //TODO Check this
+            ($(element).get(0) as any).contentWindow.postMessage(JSON.stringify(data), '*');
+            //});
+        });
+    }
+
+    static factory(): ng.IDirectiveFactory {
+        return ($rootScope: IRootScope, $document: ng.IDocumentService, $window: ng.IWindowService, Paths: IPaths) => new SocialIFrame($rootScope, $window);
+    }
+
+}
+
+angular.module('myApp.directives').directive('socialIframe', SocialIFrame.factory());
