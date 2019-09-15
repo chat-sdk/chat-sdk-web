bbde80f1a3efa9f48ed51e7d94abb886d318ef54
Pepe Becker <mail@pepebecker.com>
2019-09-15 16:34:17 +0900

    Update RoomOpenQueue service

--- src/angularjs/services/room-open-queue.ts
+++ src/angularjs/services/room-open-queue.ts
@@ -1,4 +1,5 @@
-import * as angular from 'angular'
+import * as angular from 'angular';
+
 /**
  * This service allows us to flag a room to be opened. This
  * is useful because when we create a new room it's turned
@@ -6,10 +7,9 @@ import * as angular from 'angular'
  * to flag it to be opened from anywhere and then let
  * that function open it
  */
-
 export interface IRoomOpenQueue {
-    roomExistsAndPop(rid: string): boolean
-    addRoomWithID(rid: string): void
+    roomExistsAndPop(rid: string): boolean;
+    addRoomWithID(rid: string): void;
 }
 
 class RoomOpenQueue implements IRoomOpenQueue {
@@ -17,14 +17,14 @@ class RoomOpenQueue implements IRoomOpenQueue {
     rids = new Array<string>();
 
     addRoomWithID(rid: string): void {
-        if(this.rids.indexOf(rid) < 0) {
+        if (this.rids.indexOf(rid) < 0) {
             this.rids.push(rid);
         }
     }
 
-    roomExistsAndPop (rid: string): boolean {
+    roomExistsAndPop(rid: string): boolean {
         let index = this.rids.indexOf(rid);
-        if(index >= 0) {
+        if (index >= 0) {
             this.rids.splice(index, 1);
             return true;
         }
