56f1c9edaf978faec8a7b7e042b9288df05117d5
Pepe Becker <mail@pepebecker.com>
2019-09-17 18:26:20 +0900

    Update FirebaseUploadHandler service

--- src/angularjs/network/firebase-upload-handler.ts
+++ src/angularjs/network/firebase-upload-handler.ts
@@ -1,9 +1,10 @@
 import * as angular from 'angular'
 import * as firebase from 'firebase';
-import {IUploadHandler} from "./upload-handler";
+
+import { IUploadHandler } from './upload-handler';
 
 export interface IFirebaseUploadHandler extends IUploadHandler{
-    uuid(): string
+    uuid(): string;
 }
 
 class FirebaseUploadHandler implements IFirebaseUploadHandler {
@@ -16,30 +17,29 @@ class FirebaseUploadHandler implements IFirebaseUploadHandler {
         this.$q = $q;
     }
 
-    uploadFile(file: File): Promise<any> {
+    async uploadFile(file: File): Promise<any> {
         // Create a root reference
         const storageRef = firebase.storage().ref();
 
         // Create a reference to 'mountains.jpg'
         const ref = storageRef.child('web/' + this.uuid() + '.jpg');
 
-        return ref.put(file).then(() => {
-            return ref.getDownloadURL().then((downloadURL) => {
-                console.log('File available at', downloadURL);
-            });
-        });
+        await ref.put(file);
+        const downloadURL = await ref.getDownloadURL();
+        console.log('File available at', downloadURL);
     }
 
     uuid(): string {
-        function s4() {
+        const s4 = () => {
             return Math.floor((1 + Math.random()) * 0x10000)
                 .toString(16)
                 .substring(1);
         }
 
         return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
+               s4() + '-' + s4() + s4() + s4();
     }
+
 }
 
 angular.module('myApp.services').service('FirebaseUploadHandler', FirebaseUploadHandler);
