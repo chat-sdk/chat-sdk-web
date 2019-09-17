4b2865645ee49c91f7c972f121f09e3a3bf325a2
Pepe Becker <mail@pepebecker.com>
2019-09-17 19:59:31 +0900

    Update FriendConnector service

--- src/angularjs/connectors/friend-connector.ts
+++ src/angularjs/connectors/friend-connector.ts
@@ -1,115 +1,127 @@
-import * as angular from 'angular'
-import {Utils} from "../services/utils";
-import {N} from "../keys/notification-keys";
-import {IUser} from "../entities/user";
+import * as angular from 'angular';
+
+import { Utils } from '../services/utils';
+import { N } from '../keys/notification-keys';
+import { IUser } from '../entities/user';
+import { IRootScope } from '../interfaces/root-scope';
+import { IUserStore } from '../persistence/user-store';
+import { IPaths } from '../network/paths';
 
 export interface IFriendsConnector {
-    on(uid: string): void
-    off(uid: string): void
-    isFriend(user: IUser): boolean
+    on(uid: string): void;
+    off(uid: string): void;
+    isFriend(user: IUser): boolean;
 }
 
-angular.module('myApp.services').factory('FriendsConnector', ['$rootScope', 'User', 'UserStore', 'Paths', function ($rootScope, User, UserStore, Paths) {
-    return {
+class FriendsConnector implements FriendsConnector {
 
-        friends: {},
+    static $inject = ['$rootScope', 'User', 'UserStore', 'Paths'];
 
-        on: function (uid: string): void {
-            let friendsRef = Paths.userFriendsRef(uid);
+    friends: { [uid: string]: IUser } = {};
 
-            friendsRef.on('child_added', (snapshot) => {
+    constructor(
+        private $rootScope: IRootScope,
+        private User: IUser,
+        private UserStore: IUserStore,
+        private Paths: IPaths,
+    ) { }
 
-                if(snapshot && snapshot.val()) {
-                    this.impl_friendAdded(snapshot);
-                }
+    on(uid: string) {
+        let friendsRef = this.Paths.userFriendsRef(uid);
 
-            });
+        friendsRef.on('child_added', (snapshot) => {
 
-            friendsRef.on('child_removed', (snapshot) => {
-
-                if(snapshot && snapshot.val()) {
-                    this.impl_friendRemoved(snapshot);
-                }
-
-            });
-        },
-
-        off: function (uid: string): void {
-            let friendsRef = Paths.userFriendsRef(uid);
-
-            friendsRef.off('child_added');
-            friendsRef.off('child_removed');
-
-            this.friends = {};
-        },
-
-        /**
-         * Friends
-         */
-
-        impl_friendAdded: function (snapshot) {
-
-            let uid = snapshot.val().uid;
-            if(uid) {
-                let user = UserStore.getOrCreateUserWithID(uid);
-
-                user.removeFriend = function () {
-                    snapshot.ref.remove();
-                };
-                this.addFriend(user);
+            if (snapshot && snapshot.val()) {
+                this.impl_friendAdded(snapshot);
             }
 
-        },
+        });
 
-        impl_friendRemoved: function (snapshot) {
-            this.removeFriendWithID(snapshot.val().uid);
-        },
+        friendsRef.on('child_removed', (snapshot) => {
 
-        addFriendsFromConfig: function (friends) {
-            for(let i = 0; i < friends.length; i++) {
-                let uid = friends[i];
-
-                let user = UserStore.getOrCreateUserWithID(uid);
-                user.ssoFriend = true;
-
-                this.addFriend(user);
+            if (snapshot && snapshot.val()) {
+                this.impl_friendRemoved(snapshot);
             }
-        },
 
-        addFriend: function (user) {
-            if(user && user.uid()) {
-                this.friends[user.uid()] = user;
-                user.friend = true;
-                $rootScope.$broadcast(N.FriendAdded);
+        });
+    }
+
+    off(uid: string) {
+        let friendsRef = this.Paths.userFriendsRef(uid);
+
+        friendsRef.off('child_added');
+        friendsRef.off('child_removed');
+
+        this.friends = {};
+    }
+
+    // Friends
+
+    impl_friendAdded(snapshot: firebase.database.DataSnapshot) {
+
+        const uid: string = snapshot.val().uid;
+        if (uid) {
+            const user = this.UserStore.getOrCreateUserWithID(uid);
+
+            user.removeFriend = () => {
+                snapshot.ref.remove();
             }
-        },
+            this.addFriend(user);
+        }
 
-        isFriend: function (user: IUser): boolean {
-            if(user && user.uid()) {
-                return this.isFriendUID(user.uid());
-            }
-            return false;
-        },
+    }
 
-        isFriendUID: function(uid) {
-            return !Utils.unORNull(this.friends[uid]);
-        },
+    impl_friendRemoved(snapshot: firebase.database.DataSnapshot) {
+        this.removeFriendWithID(snapshot.val().uid);
+    }
 
-        removeFriend: function (user) {
-            if(user && user.uid()) {
-                this.removeFriendWithID(user.uid());
-            }
-        },
+    addFriendsFromConfig(friends: string[]) {
+        for(let i = 0; i < friends.length; i++) {
+            const uid = friends[i];
 
-        removeFriendWithID: function (uid) {
-            if(uid) {
-                let user = this.friends[uid];
-                if(user) {
-                    user.friend = false;
-                    delete this.friends[uid];
-                    $rootScope.$broadcast(N.FriendRemoved);
-                }
+            const user = this.UserStore.getOrCreateUserWithID(uid);
+            user.ssoFriend = true;
+
+            this.addFriend(user);
+        }
+    }
+
+    addFriend(user: IUser) {
+        if (user && user.uid()) {
+            this.friends[user.uid()] = user;
+            user.friend = true;
+            this.$rootScope.$broadcast(N.FriendAdded);
+        }
+    }
+
+    isFriend(user: IUser): boolean {
+        if (user && user.uid()) {
+            return this.isFriendUID(user.uid());
+        }
+        return false;
+    }
+
+    isFriendUID(uid: string): boolean {
+        return !Utils.unORNull(this.friends[uid]);
+    }
+
+    removeFriend(user: IUser) {
+        if (user && user.uid()) {
+            this.removeFriendWithID(user.uid());
+        }
+    }
+
+    removeFriendWithID(uid: string) {
+        if (uid) {
+            const user = this.friends[uid];
+            if (user) {
+                user.friend = false;
+                delete this.friends[uid];
+                this.$rootScope.$broadcast(N.FriendRemoved);
             }
         }
     }
-}]);
+
+}
+
+angular.module('myApp.services').service('FriendsConnector', FriendsConnector);
